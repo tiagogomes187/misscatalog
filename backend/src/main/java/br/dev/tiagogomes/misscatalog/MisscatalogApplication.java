a7de6d375ecc73b693ae9c9a4f960f76006d0225
Tiago Gomes <contato@tiagogomes.dev.br>
2025-02-17 21:18:42 -0300

    Project created

---
+++ backend/src/main/java/br/dev/tiagogomes/misscatalog/MisscatalogApplication.java
@@ -0,0 +1,13 @@
+package br.dev.tiagogomes.misscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisscatalogApplication.class, args);
+	}
+
+}
